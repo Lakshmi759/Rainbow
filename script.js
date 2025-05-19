70380c20226cabfe17c40d2681ada3ec5efb5728
Kammara Lakshmi <kammaralakshmi9@gmail.com>
2025-05-19 19:54:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("red").onclick = function () {
+    document.body.style.backgroundColor = "#f44336";
+    document.querySelector("h1").style.color = "white";
+};
+document.getElementById("orange").onclick = function () {
+    document.body.style.backgroundColor = "#ff9800";
+};
+document.getElementById("yellow").onclick = function () {
+    document.body.style.backgroundColor = "#ffeb3b";
+};
+document.getElementById("green").onclick = function () {
+    document.body.style.backgroundColor = "#2ecc71";
+};
+document.getElementById("blue").onclick = function () {
+    document.body.style.backgroundColor = "#2196f3";
+};
+document.getElementById("indigo").onclick = function () {
+    document.body.style.backgroundColor = "#673ab7";
+};
+document.getElementById("violet").onclick = function () {
+    document.body.style.backgroundColor = "#9c27b0";
+};
